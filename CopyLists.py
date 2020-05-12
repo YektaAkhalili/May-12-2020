42c0a51e361e258ec3517848680d98bf02781a84
YektaAkhalili <50711176+YektaAkhalili@users.noreply.github.com>
2020-05-12 11:20:27 +0430

    Add files via upload

---
+++ CopyLists.py
@@ -0,0 +1,6 @@
+Delos = ["Charlotte", "William", "James", "Logan"]
+Guests = Delos[:]
+# Guests.append("Emily")
+Guests.insert(2,'Emily')
+
+print("Guests at the park are: ", Guests)
---
+++ tuples.py
@@ -0,0 +1,11 @@
+guests = ("William", "Charlotte")
+# print("First person: ", guests[0])
+
+print("original guests: ")
+for people in guests:
+	print(people)
+
+guests = ("HostWilliam", "Holores")
+print("replaced guests: ")	
+for people in guests:
+	print(people)
